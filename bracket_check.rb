dbd977da75ebe12f5f664b51ddd6ec74af407958
y-kida11 <y.kida@gingerapp.co.jp>
2017-12-28 17:22:30 +0900

    括弧の整合性確認問題を回答

---
+++ bracket_check.rb
@@ -0,0 +1,31 @@
+class BracketCheck
+  def self.run(str)
+    new(str).run
+  end
+
+  def initialize(str)
+    @chars = str.split('')
+    @stack = []
+    @brackets = { '{' => '}', '(' => ')', '[' => ']', '<' => '>' }
+  end
+
+  def run
+    bracket_existance = false
+    chars.each do |char|
+      case char
+      when '(', '{', '[', '<'
+        bracket_existance = true
+        stack.push(char)
+      when ')', '}', ']', '>'
+        return puts 'NG' unless brackets[stack.pop] == char
+      end
+    end
+    puts bracket_existance ? 'OK' : 'NG'
+  end
+
+  private
+
+  attr_reader :chars, :stack, :brackets, :bracket_existance
+end
+
+BracketCheck.run(ARGV[0])
